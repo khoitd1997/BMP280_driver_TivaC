e975e97e4b8105f934b560bb2984a08bc786d169
khoitd1997 <khoidinhtrinh@gmail.com>
2017-12-23 18:55:09 -0800

    Added reference, .clang-format and readme

---
+++ BMP280_main_tivaC.c
@@ -0,0 +1,11 @@
+/**
+ * Written by Khoi Dinh Trinh on 12/13/2017
+ * BMP280 breakout board from Adafruit:
+ * https://www.adafruit.com/product/2651
+ * Reference:
+ * https://github.com/BoschSensortec/BMP280_driver
+ * https://github.com/adafruit/Adafruit_BMP280_Library
+ *
+ * This file is used for testing the Tiva C with BMP280 BoschSensortec
+ * API will be in a separate file
+ */
