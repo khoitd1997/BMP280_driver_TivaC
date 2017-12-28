28e8579ac2105277ef8cf06548a7a0de8021ee1a
khoitd1997 <khoidinhtrinh@gmail.com>
2017-12-28 12:54:17 -0800

    --

--- main.c
+++ main.c
@@ -13,7 +13,21 @@
 
 File    : main.c
 Purpose : Generic application start
-*/
+
+
+/**
+ * Written by Khoi Dinh Trinh on 12/13/2017
+ * BMP280 breakout board from Adafruit:
+ * https://www.adafruit.com/product/2651
+ * Reference:
+ * https://github.com/BoschSensortec/BMP280_driver
+ * https://github.com/adafruit/Adafruit_BMP280_Library
+ *
+ * This file is used for testing the Tiva C with BMP280 BoschSensortec
+ * API will be in a separate file
+ */
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,3 +51,4 @@ void main(void) {
 }
 
 /*************************** End of file ****************************/
+dsadasd
