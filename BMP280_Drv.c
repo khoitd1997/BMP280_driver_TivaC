5206a4b872adce40e2ec1d112f2e9fcf1ac2257a
khoitd1997 <khoidinhtrinh@gmail.com>
2018-02-13 22:41:17 -0800

    make new git local repo and added init function for sensor

--- BMP280_Drv.c
+++ BMP280_Drv.c
@@ -1,2 +1,66 @@
 #include "BMP280_Drv.h"
+#include "TivaC_I2C.h"
 
+void bmp280Init(bmp280* sensor, bmp280_measureSettings settings, bmp280_comProtocol protocol)
+{
+    //values in settings obtained from BMP280 datasheet page 19
+    sensor->protocol=protocol;
+    switch(settings)
+    {
+        case handLow:
+            sensor-> mode=Normal;
+            sensor-> tempSamp= x2;
+            sensor-> presSamp= x16;
+            sensor-> samplSet= UltraHigh;
+            sensor->iirFilter=x4;
+            sensor->standbyTime=62.5;
+            break;
+        case handDynamic:
+            sensor-> mode=Normal;
+            sensor-> tempSamp= x1;
+            sensor-> presSamp= x4;
+            sensor-> samplSet= Standard;
+            sensor->iirFilter=x16;
+            sensor->standbyTime=0.5;
+            break;
+        case weatherStat:
+            sensor-> mode=Forced;
+            sensor-> tempSamp= x1;
+            sensor-> presSamp= x1;
+            sensor-> samplSet= UltraLow;
+            sensor->iirFilter= x0;
+            //0.05 is placeholder for now
+            sensor->standbyTime=0.05;
+            break;
+        case elevDetec:
+            sensor-> mode=Normal;
+            sensor-> tempSamp= x1;
+            sensor-> presSamp= x4;
+            sensor-> samplSet= Standard;
+            sensor->iirFilter= x4;
+            sensor->standbyTime=125; 
+            break;
+        case dropDetec:
+            sensor-> mode=Normal;
+            sensor-> tempSamp= x1;
+            sensor-> presSamp= x2;
+            sensor-> samplSet= Low;
+            sensor->iirFilter= x0;
+            sensor->standbyTime=0.5;
+            break;
+        case indoorNav:
+            sensor-> mode=Normal;
+            sensor-> tempSamp= x2;
+            sensor-> presSamp= x16;
+            sensor-> samplSet= UltraHigh;
+            sensor->iirFilter=x16;
+            sensor->standbyTime=0.5;
+            break;
+        default:
+        //stop the mcu if none of these options for now
+            for(;;)
+    }
+}
+uint8_t bmp280_getID(bmp280);
+void bmp280_reset(bmp280);
+void bmp280_init(bmp280);
--- BMP280_Drv.h
+++ BMP280_Drv.h
@@ -1,12 +1,17 @@
 #ifndef _BMP280_DRV_
 #define _BMP280_DRV_
 
+#include <stdint.h>
 //enum of all the sensors' settings
-enum comProtocol {SPI, I2C};
+typedef enum {SPI, I2C} bmp280_comProtocol;
 
 //measurement settings based on the datasheet of BMP280
-enum measureSettings{Custom, handLow, handDynamic, weatherStat, elevDetec, dropDetec, indoorNav};
-enum operMode{Sleep, Forced, Normal};
+//may just ask user to enter the settings
+typedef enum {handLow, handDynamic, weatherStat, elevDetec, dropDetec, indoorNav} bmp280_measureSettings;
+typedef enum {Sleep, Forced, Normal} bmp280_operMode;
+typedef enum {UltraLow, Low, Standard, UltraHigh} bmp280_samplSettings;
+//used for IIR filter, temp and pressure oversampling coefficience
+typedef enum{x0, x1, x2, x4, x8, x16} bmp280_Coeff;
 
 //use log for array indexing in oversampling
 
@@ -16,12 +21,30 @@ typedef struct bmp280Sensor bmp280;
 //represent one bmp280 sensor along with its settings
 struct bmp280Sensor 
 {
+    uint8_t ID;
+    uint8_t address;
+    bmp280_comProtocol protocol;
+    //I2C or SPI controller is it assigned to
+    uint8_t portNum;
 
+    //temperature and pressure oversampling settings
+    bmp280_Coeff tempSamp;
+    bmp280_Coeff presSamp;
+    bmp280_Coeff iirFilter;
+    bmp280_samplSettings samplSet;
+    bmp280_operMode mode;
+
+    //unit is ms 
+    float standbyTime;
 };
 
 //initialize a bmp280 struct
 //will be use in a big switch statement for initializing based on
 //both predefined option and customizable value 
-struct bmp280Sensor bmp280Init();
+void bmp280Init(bmp280*, bmp280_measureSettings,bmp280_comProtocol);
+uint8_t bmp280_getID(bmp280*);
+void bmp280_reset(bmp280*);
+void bmp280_init(bmp280*);
+
 
 #endif
