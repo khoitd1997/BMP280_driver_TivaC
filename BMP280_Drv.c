09bc35fcd91932d1eaa22d8ccf564996cfee7cf5
khoitd1997 <khoidinhtrinh@gmail.com>
2018-03-25 17:22:27 -0700

    get ID works added function prototypes

--- BMP280_Drv.c
+++ BMP280_Drv.c
@@ -1,111 +1,146 @@
 /**
  * This is the main driver file for bmp280 but need i2c file from tivaC to work
- * 
+ *
  */
-
 
 #include "BMP280_Drv.h"
 #include "TivaC_I2C.h"
 
-typedef enum{status=0, ctrl_meas, config, press_msb, press_lsb, press_xlsb, temp_msb, temp_lsb, temp_xlsb} bmp280_regName;
+typedef enum {
+  status = 0,
+  ctrl_meas,
+  config,
+  press_msb,
+  press_lsb,
+  press_xlsb,
+  temp_msb,
+  temp_lsb,
+  temp_xlsb
+} bmp280_regName;
 
-//added with the bmp280_regName to get the correct address
-static uint16_t bmp280_baseRegAddr=0xf3;
+// added with the bmp280_regName to get the correct address
+static uint16_t bmp280_baseRegAddr = 0xf3;
 
-//the two special addresses
-static uint16_t bmp280_resAddr=0xe0;
-static uint16_t bmp280_idAddr=0xd0;
+// the two special addresses
+static uint16_t bmp280_resAddr = 0xe0;
+static uint16_t bmp280_idAddr  = 0xd0;
 
-//initialize the bmp280 with predefined value in the datasheet
-void bmp280_init(bmp280* sensor, bmp280_measureSettings settings, bmp280_comProtocol protocol)
+// initialize the bmp280 with predefined value in the datasheet
+void bmp280_init(bmp280*                sensor,
+                 bmp280_measureSettings settings,
+                 bmp280_comProtocol     protocol)
 {
-
-    //value pretty much hardcoded due to hardware
-    sensor->address=0x77;
-    //values in settings obtained from BMP280 datasheet page 19
-    sensor->protocol=protocol;
-    switch(settings)
+  sensor->portNum = 0;     // 0 for now
+  sensor->address = 0x77;  // value pretty much hardcoded due to hardware
+  sensor->protocol =
+      protocol;  // values in settings obtained from BMP280 datasheet page 19
+  switch (settings)
     {
-        case handLow:
-            sensor-> mode=Normal;
-            sensor-> tempSamp= x2;
-            sensor-> presSamp= x16;
-            sensor-> samplSet= UltraHigh;
-            sensor->iirFilter=x4;
-            sensor->standbyTime=62.5;
-            break;
-        case handDynamic:
-            sensor-> mode=Normal;
-            sensor-> tempSamp= x1;
-            sensor-> presSamp= x4;
-            sensor-> samplSet= Standard;
-            sensor->iirFilter=x16;
-            sensor->standbyTime=0.5;
-            break;
-        case weatherStat:
-            sensor-> mode=Forced;
-            sensor-> tempSamp= x1;
-            sensor-> presSamp= x1;
-            sensor-> samplSet= UltraLow;
-            sensor->iirFilter= x0;
-            //0.05 is placeholder for now
-            sensor->standbyTime=0.05;
-            break;
-        case elevDetec:
-            sensor-> mode=Normal;
-            sensor-> tempSamp= x1;
-            sensor-> presSamp= x4;
-            sensor-> samplSet= Standard;
-            sensor->iirFilter= x4;
-            sensor->standbyTime=125; 
-            break;
-        case dropDetec:
-            sensor-> mode=Normal;
-            sensor-> tempSamp= x1;
-            sensor-> presSamp= x2;
-            sensor-> samplSet= Low;
-            sensor->iirFilter= x0;
-            sensor->standbyTime=0.5;
-            break;
-        case indoorNav:
-            sensor-> mode=Normal;
-            sensor-> tempSamp= x2;
-            sensor-> presSamp= x16;
-            sensor-> samplSet= UltraHigh;
-            sensor->iirFilter=x16;
-            sensor->standbyTime=0.5;
-            break;
-        default:
-            for(;;)
-            {
-                //stop the mcu if none of these options for now
-            }
+      case handLow:
+        sensor->mode        = Normal;
+        sensor->tempSamp    = x2;
+        sensor->presSamp    = x16;
+        sensor->samplSet    = UltraHigh;
+        sensor->iirFilter   = x4;
+        sensor->standbyTime = 62.5;
+        break;
+      case handDynamic:
+        sensor->mode        = Normal;
+        sensor->tempSamp    = x1;
+        sensor->presSamp    = x4;
+        sensor->samplSet    = Standard;
+        sensor->iirFilter   = x16;
+        sensor->standbyTime = 0.5;
+        break;
+      case weatherStat:
+        sensor->mode      = Forced;
+        sensor->tempSamp  = x1;
+        sensor->presSamp  = x1;
+        sensor->samplSet  = UltraLow;
+        sensor->iirFilter = x0;
+        // 0.05 is placeholder for now
+        sensor->standbyTime = 0.05;
+        break;
+      case elevDetec:
+        sensor->mode        = Normal;
+        sensor->tempSamp    = x1;
+        sensor->presSamp    = x4;
+        sensor->samplSet    = Standard;
+        sensor->iirFilter   = x4;
+        sensor->standbyTime = 125;
+        break;
+      case dropDetec:
+        sensor->mode        = Normal;
+        sensor->tempSamp    = x1;
+        sensor->presSamp    = x2;
+        sensor->samplSet    = Low;
+        sensor->iirFilter   = x0;
+        sensor->standbyTime = 0.5;
+        break;
+      case indoorNav:
+        sensor->mode        = Normal;
+        sensor->tempSamp    = x2;
+        sensor->presSamp    = x16;
+        sensor->samplSet    = UltraHigh;
+        sensor->iirFilter   = x16;
+        sensor->standbyTime = 0.5;
+        break;
+      case custom:
+        // do nothing and let user does the config themselves
+      default:
+        for (;;)
+          {
+            // stop the mcu if none of these options for now
+          }
     }
 }
 
-//get manufacturer ID of the bmp280
+// get manufacturer ID of the bmp280
 uint8_t bmp280_getID(bmp280* sensor)
 {
-    uint8_t ID;
-    i2c0_open();
-    //write data with no stop signal
-    i2c0_single_data_write(sensor->address, bmp280_idAddr, 0);
-    //repeat start and then read the ID 
-    ID=i2c0_single_data_read(sensor->address, 0,1);
-    i2c0_close();
-    return ID;
+  uint8_t ID;
+  if (sensor->protocol == I2C)
+    {
+      if (!sensor->portOpened)
+        {
+          i2c0_open();
+        }
+      else
+        {
+          i2c0_waitBusy();
+        }
+      // write data with no stop signal
+      i2c0_single_data_write(sensor->address, bmp280_idAddr, 0);
+      // repeat start and then read the ID
+      ID = i2c0_single_data_read(sensor->address, 0, 1);
+      i2c0_close();
+    }
+  sensor->ID = ID;
+  return ID;
 }
 
-//reset the bmp280 with a power-on reset
+// reset the bmp280 with a power-on reset
 void bmp280_reset(bmp280* sensor)
 {
-    uint8_t resSeq[2];
-    resSeq[0]=bmp280_resAddr;
+  uint8_t resSeq[2];
+  resSeq[0] = bmp280_resAddr;
 
-    //obtain from page 24 datasheet
-    resSeq[0]=0xb6;
-    i2c0_open();
-    i2c0_multiple_data_byte_write(sensor->address, resSeq, 2);
-    i2c0_close();
+  // obtain from page 24 datasheet
+  resSeq[0] = 0xb6;
+
+  if (sensor->protocol == I2C)
+    {
+      if (!sensor->portOpened)
+        {
+          i2c0_open();
+        }
+      else
+        {
+          i2c0_waitBusy();
+        }
+      i2c0_multiple_data_byte_write(sensor->address, resSeq, 2);
+      i2c0_close();
+    }
+
+  // TODO: do SPI
 }
-
--- BMP280_Drv.h
+++ BMP280_Drv.h
@@ -3,51 +3,69 @@
 
 #include <stdint.h>
 
+// enum of all the sensors' settings
+typedef enum { SPI, I2C } bmp280_comProtocol;
 
-//enum of all the sensors' settings
-typedef enum {SPI, I2C} bmp280_comProtocol;
+// measurement settings based on the datasheet of BMP280
+// may just ask user to enter the settings
+typedef enum {
+  handLow = 0,
+  handDynamic,
+  weatherStat,
+  elevDetec,
+  dropDetec,
+  indoorNav,
+  custom
+} bmp280_measureSettings;
+typedef enum { Sleep = 0, Forced, Normal } bmp280_operMode;
+typedef enum { UltraLow = 0, Low, Standard, UltraHigh } bmp280_samplSettings;
+// used for IIR filter, temp and pressure oversampling coefficience
+typedef enum { x0 = 0, x1, x2, x4, x8, x16 } bmp280_Coeff;
 
-//measurement settings based on the datasheet of BMP280
-//may just ask user to enter the settings
-typedef enum {handLow=0, handDynamic, weatherStat, elevDetec, dropDetec, indoorNav} bmp280_measureSettings;
-typedef enum {Sleep=0, Forced, Normal} bmp280_operMode;
-typedef enum {UltraLow=0, Low, Standard, UltraHigh} bmp280_samplSettings;
-//used for IIR filter, temp and pressure oversampling coefficience
-typedef enum{x0=0, x1, x2, x4, x8, x16} bmp280_Coeff;
+// use log for array indexing in oversampling
 
-//use log for array indexing in oversampling
-
-//used for getting the address of the register in BMp280
+// used for getting the address of the register in BMp280
 
 typedef struct bmp280Sensor bmp280;
 
-//represent one bmp280 sensor along with its settings
-struct bmp280Sensor 
+// represent one bmp280 sensor along with its settings
+struct bmp280Sensor
 {
-    uint8_t ID;
-    uint8_t address;
-    bmp280_comProtocol protocol;
-    //I2C or SPI controller is it assigned to
-    uint8_t portNum;
+  int     portOpened;  // status flag to see if the I2C/SPI port is open
+  uint8_t ID;
+  uint8_t address;
+  bmp280_comProtocol protocol;
+  uint8_t            portNum;  // I2C or SPI controller is it assigned to
 
-    //temperature and pressure oversampling settings
-    bmp280_Coeff tempSamp;
-    bmp280_Coeff presSamp;
-    bmp280_Coeff iirFilter;
-    bmp280_samplSettings samplSet;
-    bmp280_operMode mode;
+  // temperature and pressure oversampling settings
+  bmp280_Coeff         tempSamp;
+  bmp280_Coeff         presSamp;
+  bmp280_Coeff         iirFilter;
+  bmp280_samplSettings samplSet;
+  bmp280_operMode      mode;
 
-    //unit is ms 
-    float standbyTime;
+  // unit is ms
+  float standbyTime;
 };
 
-//initialize a bmp280 struct
-//will be use in a big switch statement for initializing based on
-//both predefined option and customizable value 
-void bmp280_init(bmp280* sensor, bmp280_measureSettings settings, bmp280_comProtocol protocol);
-uint8_t bmp280_getID(bmp280*);
-void bmp280_reset(bmp280*);
+// initialize a bmp280 struct
+// will be use in a big switch statement for initializing based on
+// both predefined option and customizable value
+void bmp280_init(bmp280*                sensor,
+                 bmp280_measureSettings settings,
+                 bmp280_comProtocol     protocol);
+void bmp280_open(bmp280* sensor); // open the communication channel on I2C/SPI bus
+void bmp280_close(bmp280* sensor); 
 
+// functions used for customizing the setting of the bmp280
+// calling these functions will cause a write to the actual hardware
+void bmp280_set_temp(bmp280* sensor);
+void bmp280_set_pres(bmp280* sensor);
+void bmp280_set_filter(bmp280* sensor);
+void bmp280_set_sampling(bmp280* sensor);
+void bmp280_set_mode(bmp280* sensor);
 
+uint8_t bmp280_getID(bmp280* sensor);
+void    bmp280_reset(bmp280* sensor);
 
 #endif
