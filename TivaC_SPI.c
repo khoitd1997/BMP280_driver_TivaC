672870cebf7cd64147ec8cf81114dc3bd19d513c
khoitd1997 <khoidinhtrinh@gmail.com>
2018-04-13 01:08:04 -0700

    added GPIO config for SPI

--- TivaC_SPI.c
+++ TivaC_SPI.c
@@ -1,11 +1,46 @@
 #include <assert.h>
 
-#include "tm4c123gh6pm.h"
 #include "TivaC_SPI.h"
-
+#include "tm4c123gh6pm.h"
 
 void spi_open(const spi_settings* setting, spi_errCode* errCode)
 {
-    assert(setting);
-    assert(errCode);
+  assert(setting);
+  assert(errCode);
+
+  // TODO: add support for SPI controller other spi0
+  if (setting->spiControlerNum == 0)
+    {
+      SYSCTL_RCGCSSI_R |= SYSCTL_RCGCSSI_R0;    // turn on SPI module
+      SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R0;  // enable clock SPI pins
+
+      // need afsel for 19(PA2)(clock), 20(frame signal), 21(RX), 22(TX)
+      GPIO_PORTA_AFSEL_R &= ~0x0000003C;
+
+      // PMCn for the pins above to 2
+      GPIO_PORTA_PCTL_R &= ~0xFFDDDDFF;
+      GPIO_PORTA_PCTL_R |= 0x00222200;
+
+      // GPIODEN for all those pins
+      GPIO_PORTA_DEN_R |= 0x0000003C;
+
+      GPIO_PORTA_DR4R_R |= 0x0000003C;  // 4 mA drive Strength
+      GPIO_PORTA_ODR_R |= 0x0000003C;   // open drain enabled
+
+      //  pull up/ pull down pull-up for clock pin if high steady state in SPO
+      if (setting->cpol == 0)
+        {
+          GPIO_PORTA_PDR_R |= 0x00000004;
+        }
+      else if (setting->cpol == 1)
+        {
+          GPIO_PORTA_PUR_R |= 0x00000004;
+        }
+      else
+        {
+          // TODO: change this into error handling func
+          *errCode = ERROR_SPI_CPA_UNDEFINED;
+          return;
+        }
+    }
 }
--- TivaC_SPI.h
+++ TivaC_SPI.h
@@ -4,26 +4,35 @@
 
 /* Note that this assumes we are using FreeScale SPI (so 4-wire) */
 
-typedef enum
-{
-
+typedef enum {
+  ERROR_SPI_CPA_UNDEFINED,
 } spi_errCode;
 
-typedef struct 
+typedef enum { Freescale, Tissf, Microwire } spi_protocol_mode;
+
+typedef struct
 {
-    uint8_t cpol;
-    uint8_t cpha; 
-    uint8_t isFourWire;
-    uint8_t isMaster;
-    // TODO: maybe add interrupts support
+  uint8_t spiControlerNum;
+  uint8_t clockRateMHz;
+  uint8_t cpol;
+  uint8_t cpha;
+  spi_protocol_mode operMode;
+  uint8_t dataSize;
+  uint8_t isMaster;
+  // TODO: maybe add interrupts support
 } spi_settings;
 
 /* Communication setup */
-void spi_open(const spi_settings* setting, spi_errCode* errCode); // setup all necessary register, but don't start communication until spi_transfer is called
-void spi_close(spi_errCode* errCode); // disable the spi controller and clean up register
+void spi_open(const spi_settings* setting,
+              spi_errCode* errCode);  // setup all necessary register, but don't
+                                      // start communication until spi_transfer
+                                      // is called
+void spi_close(
+    spi_errCode* errCode);  // disable the spi controller and clean up register
 
 /* Data Transfer */
-void spi_transfer(uint8_t* data, uint8_t dataLength); // used for both single and multiple bytes
-
+void spi_transfer(
+    uint8_t* data,
+    uint8_t  dataLength);  // used for both single and multiple bytes
 
 #endif
